efcc879d464f5322dfce45c94654b7ba89c10f3d
George <george.chirikov@metropolia.fi>
2025-03-28 17:10:32 +0200

    refactor: initialized new kotlin files in dao, dto, database, model, view and viewmodel

--- app/src/main/java/com/example/foodiediary/data/dao/UserDto.kt
+++ app/src/main/java/com/example/foodiediary/data/dao/UserDto.kt
@@ -1,4 +1,29 @@
 package com.example.foodiediary.data.dao
 
-class UserDto {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.foodiediary.models.entity.User
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user: User)
+
+    @Query("SELECT * FROM users WHERE userId = :userId LIMIT 1")
+    suspend fun getUserById(userId: Long): User?
+
+    @Query("SELECT * FROM users WHERE username = :username LIMIT 1")
+    suspend fun getUserByUsername(username: String): User?
+
+    @Query("SELECT * FROM users")
+    fun getAllUsers(): LiveData<List<User>>
+
+    @Delete
+    suspend fun deleteUser(user: User)
 }
+
--- app/src/main/java/com/example/foodiediary/data/repository/UserReporitory.kt
+++ app/src/main/java/com/example/foodiediary/data/repository/UserReporitory.kt
@@ -1,4 +1,26 @@
 package com.example.foodiediary.data.repository
 
-class UserReporitory {
+import androidx.lifecycle.LiveData
+import com.example.foodiediary.data.dao.UserDao
+import com.example.foodiediary.models.entity.User
+
+class UserRepository(private val userDao: UserDao) {
+
+    val allUsers: LiveData<List<User>> = userDao.getAllUsers()
+
+    suspend fun insertUser(user: User) {
+        userDao.insertUser(user)
+    }
+
+    suspend fun getUserById(userId: Long): User? {
+        return userDao.getUserById(userId)
+    }
+
+    suspend fun getUserByUsername(username: String): User? {
+        return userDao.getUserByUsername(username)
+    }
+
+    suspend fun deleteUser(user: User) {
+        userDao.deleteUser(user)
+    }
 }
