62f8022312dfb30407ae5da0b5f6d9d94fb11a9d
Cindy <cindy.zhou@metropolia.fi>
2025-04-03 21:15:25 +0300

    Color theme variables added

--- app/src/main/java/com/example/foodiediary/ui/theme/Color.kt
+++ app/src/main/java/com/example/foodiediary/ui/theme/Color.kt
@@ -2,6 +2,9 @@ package com.example.foodiediary.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Color palette, each color can have their own value and be used in the app
+
+// Default colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
@@ -9,3 +12,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val PureBlack = Color(0xFF000000)
+
+
+// Our own theme
+val GrassGreen = Color(0xFF7AC74F)
+val LightGreen = Color(0xFFA1CF6B)
+val ShyGreen = Color(0xFFC5E1A3)
+
+val RoseRed = Color(0xFFBC4749)
+val AppleRed = Color(0xFFE87461)
+
+val YellowStone = Color(0xFFE0C879)
+val NatureWhite = Color(0xFFFEFAE0)
