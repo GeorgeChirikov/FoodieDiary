751ea2f139f70677332e80ab3770155ee3eb594c
Cindy <cindy.zhou@metropolia.fi>
2025-04-09 14:20:48 +0300

    Better structure with drawer feature to all screens

--- app/src/main/java/com/example/foodiediary/MainActivity.kt
+++ app/src/main/java/com/example/foodiediary/MainActivity.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
@@ -28,19 +31,20 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.foodiediary.ui.theme.FoodieDiaryTheme
-import com.example.foodiediary.ui.theme.PureBlack
-import com.example.foodiediary.ui.theme.PurpleGrey40
 import kotlinx.coroutines.launch
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import com.example.foodiediary.ui.theme.AppleRed
 import com.example.foodiediary.ui.theme.GrassGreen
 import com.example.foodiediary.ui.theme.LightGreen
 import com.example.foodiediary.ui.theme.PureWhite
+import com.example.foodiediary.ui.theme.ShyGreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,147 +58,234 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppNavigation() {
     val navController = rememberNavController()
+    val currentRoute = navController.currentBackStackEntryFlow
+        .collectAsState(initial = navController.currentBackStackEntry)
+        .value?.destination?.route
+        ?: "home" // Default route if null
+
     NavHost(navController = navController, startDestination = "home") {
-        composable("home") { HomeScreen(navController) }
-        composable("page1") { Page1Screen() }
-        composable("page2") { Page2Screen() }
-        // Add more pages here as needed
+        composable("home") {
+            ScreenWithDrawer(navController, currentRoute) {
+                HomeScreenContent()
+            }
+        }
+        composable("page1") {
+            ScreenWithDrawer(navController, currentRoute) {
+                Page1ScreenContent()
+            }
+        }
+        composable("page2") {
+            ScreenWithDrawer(navController, currentRoute) {
+                Page2ScreenContent()
+            }
+        }
+        composable("page3") {
+            ScreenWithDrawer(navController, currentRoute) {
+                Page3ScreenContent()
+            }
+        }
+        // ... other screens ...
     }
 }
 
+//Drawer composable
 @Composable
-fun HomeScreen(navController: NavController) {
+fun ScreenWithDrawer(
+    navController: NavController,
+    currentRoute: String,
+    content: @Composable () -> Unit,
+) {
     val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
     val scope = rememberCoroutineScope()
 
     ModalNavigationDrawer(
         modifier = Modifier
-            .background(brush = Brush.verticalGradient(colors = listOf(AppleRed, LightGreen, GrassGreen))),
+            .background(AppleRed),
         drawerState = drawerState,
         drawerContent = {
-            Column(modifier = Modifier
-                .requiredWidth(140.dp)
-                .background(PurpleGrey40)
-                .padding(16.dp) ) {
-                NavigationDrawerItem(
-                    label = { Text("Page 1", modifier = Modifier.background(PureBlack)) },
-                    selected = false,
-                    onClick = {
-                        scope.launch {
-                            drawerState.close()
-                            navController.navigate("page1")
-                        }
-                    }
-                )
-                NavigationDrawerItem(
-                    label = { Text("Page 2") },
-                    selected = false,
-                    onClick = {
-                        scope.launch {
-                            drawerState.close()
-                            navController.navigate("page2")
-                        }
-                    }
-                )
-                NavigationDrawerItem(
-                    label = { Text("Close Drawer") },
-                    selected = false,
-                    onClick = {
-                        scope.launch {
-                            drawerState.close()
-                            navController.navigate("home")
-                        }
-                    }
-                )
-                // Add more items as needed
-            }
+            DrawerContent(
+                navController = navController,
+                currentRoute = currentRoute,
+                onCloseDrawer = { scope.launch { drawerState.close() } }
+            )
         },
         content = {
-            Column() {
-                Row() {
-                    Column {
-                        IconButton(modifier = Modifier.padding(15.dp), onClick = {
-                            scope.launch {
-                                drawerState.open()
-                            }
-                        }) {
-                            Icon(Icons.Filled.Menu, contentDescription = "Menu")
-                        }
-                    }
-                    Spacer(modifier = Modifier.height(30.dp))
-                    Column(modifier = Modifier
-                            .padding(20.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
-                    ) {
-                        Text("Home Screen")
-                    }
-                }
-                Spacer(modifier = Modifier.height(30.dp))
-                Column(modifier = Modifier
-                    .width(500.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
-                ) {
-                    Box(modifier = Modifier
-                        .background(PureWhite)
-                        .padding(20.dp)
-                        .width(200.dp)
-                        .height(100.dp)
-                        )
-                     {
-                            Text("Proteins, Fats, Carbs, Calories")
-                        }
-                    Spacer(modifier = Modifier.height(30.dp))
-                    Box(modifier = Modifier
-                        .background(PureWhite)
-                        .padding(20.dp)
-                        .width(200.dp)
-                        .height(100.dp)
-                        ) {
-                            Text("Water intake")
-                        }
-                    Spacer(modifier = Modifier.height(30.dp))
-                    Box(modifier = Modifier
-                        .background(PureWhite)
-                        .padding(20.dp)
-                        .width(200.dp)
-                        .height(30.dp)
-                    ) {
-                        Text("Scan items button")
-                    }
-                    Spacer(modifier = Modifier.height(30.dp))
-                    Box(modifier = Modifier
-                        .background(PureWhite)
-                        .padding(20.dp)
-                        .width(200.dp)
-                        .height(30.dp)
-                    ) {
-                        Text("History")
-                    }
-                    Spacer(modifier = Modifier.height(30.dp))
-                    Box(modifier = Modifier
-                        .background(PureWhite)
-                        .padding(20.dp)
-                        .width(200.dp)
-                        .height(30.dp)
-                    ) {
-                        Text("Favorites")
-                    }
-                }
-
+            Column {
+                TopAppBar(
+                    onMenuClick = { scope.launch { drawerState.open() } }
+                )
+                content() // Screen's content will be placed
             }
         }
     )
 }
 
 @Composable
-fun Page1Screen() {
-    Text("This is Page 1")
+fun TopAppBar(onMenuClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        IconButton(onClick = onMenuClick) {
+            Icon(Icons.Filled.Menu, contentDescription = "Menu")
+        }
+        Spacer(modifier = Modifier.width(20.dp))
+        // Other elements can be added
+    }
 }
 
 @Composable
-fun Page2Screen() {
-    Text("This is Page 2")
+fun DrawerContent(
+    navController: NavController,
+    currentRoute: String,
+    onCloseDrawer: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .requiredWidth(140.dp)
+            .background(ShyGreen)
+            .padding(16.dp)
+            .clip(RoundedCornerShape(topEnd = 20.dp, bottomEnd = 20.dp))
+    ) {
+        // Navigation items
+        DrawerItem("Home", "home", currentRoute, navController, onCloseDrawer)
+        DrawerItem("Page 1", "page1", currentRoute, navController, onCloseDrawer)
+        DrawerItem("Page 2", "page2", currentRoute, navController, onCloseDrawer)
+        DrawerItem("Page 3", "page3", currentRoute, navController, onCloseDrawer)
+    }
 }
+
+@Composable
+fun DrawerItem(
+    label: String,
+    route: String,
+    currentRoute: String,
+    navController: NavController,
+    onCloseDrawer: () -> Unit
+) {
+    NavigationDrawerItem(
+        label = { Text(label) },
+        selected = currentRoute == route,
+        onClick = {
+            onCloseDrawer()
+            if (currentRoute != route) { // Only navigate if not already on the destination
+                navController.navigate(route)
+            }
+        }
+    )
+}
+
+@Composable
+fun HomeScreenContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.verticalGradient(colors = listOf(AppleRed, LightGreen, GrassGreen)))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Home Screen Content")
+            Spacer(modifier = Modifier.height(30.dp))
+            Column(modifier = Modifier
+                .width(500.dp),
+                horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center
+            ) {
+                Box(modifier = Modifier
+                    .background(PureWhite)
+                    .padding(20.dp)
+                    .width(200.dp)
+                    .height(100.dp)
+                )
+                {
+                    Text("Proteins, Fats, Carbs, Calories")
+                }
+                Spacer(modifier = Modifier.height(30.dp))
+                Box(modifier = Modifier
+                    .background(PureWhite)
+                    .padding(20.dp)
+                    .width(200.dp)
+                    .height(100.dp)
+                ) {
+                    Text("Water intake")
+                }
+                Spacer(modifier = Modifier.height(30.dp))
+                Box(modifier = Modifier
+                    .background(PureWhite)
+                    .padding(20.dp)
+                    .width(200.dp)
+                    .height(30.dp)
+                ) {
+                    Text("Scan items button")
+                }
+                Spacer(modifier = Modifier.height(30.dp))
+                Box(modifier = Modifier
+                    .background(PureWhite)
+                    .padding(20.dp)
+                    .width(200.dp)
+                    .height(30.dp)
+                ) {
+                    Text("History")
+                }
+                Spacer(modifier = Modifier.height(30.dp))
+                Box(modifier = Modifier
+                    .background(PureWhite)
+                    .padding(20.dp)
+                    .width(200.dp)
+                    .height(30.dp)
+                ) {
+                    Text("Favorites")
+                }
+            }
+
+        }
+    }
+
+
+@Composable
+fun Page1ScreenContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.verticalGradient(colors = listOf(LightGreen, ShyGreen)))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("This is Page 1")
+    }
+}
+
+@Composable
+fun Page2ScreenContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.verticalGradient(colors = listOf(AppleRed, LightGreen, GrassGreen)))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("This is Page 1")
+    }
+}
+
+@Composable
+fun Page3ScreenContent() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = Brush.verticalGradient(colors = listOf(AppleRed, LightGreen, GrassGreen)))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("This is Page 1")
+    }
+}
+
 // Create more Composable functions for additional pages
 
 
@@ -203,6 +294,5 @@ fun Page2Screen() {
 fun MainViewPreview() {
     FoodieDiaryTheme {
         AppNavigation()
-
     }
 }
