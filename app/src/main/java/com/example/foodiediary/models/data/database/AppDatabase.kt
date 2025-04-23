382c97c711a3de0bc2e57ea260a4042a1b2fc155
George <george.chirikov@metropolia.fi>
2025-04-23 22:20:47 +0300

    feat(RoomDB): add migration for DB version management. I don't know why but didn't work

--- app/src/main/java/com/example/foodiediary/models/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/foodiediary/models/data/database/AppDatabase.kt
@@ -5,21 +5,50 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.foodiediary.models.Converters
+import com.example.foodiediary.models.data.dao.FavoriteDao
 import com.example.foodiediary.models.data.dao.ItemDao
 import com.example.foodiediary.models.data.entity.Favorite
 import com.example.foodiediary.models.data.entity.Item
 
-@Database(entities = [Item::class, Favorite::class], version = 1, exportSchema = false)
+@Database(
+    entities = [Item::class, Favorite::class],
+    version = 2,
+    exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun itemDao(): ItemDao
+    abstract fun favoriteDao(): FavoriteDao
+
 
 
     // Pitää selvittää mitä tekee
     companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
+
+        val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                // Drop the old table users
+                database.execSQL("DROP TABLE IF EXISTS `users`")
+
+                // Create the new table for favorites
+                database.execSQL("""
+                    CREATE TABLE IF NOT EXISTS `favorites` (
+                        `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+                        `ean` INTEGER NOT NULL, 
+                        `name` TEXT NOT NULL, 
+                        `protein` REAL NOT NULL, 
+                        `fat` REAL NOT NULL, 
+                        `carbohydrates` REAL NOT NULL, 
+                        `energy` REAL NOT NULL
+                    )
+                """.trimIndent())
+            }
+        }
+
         fun getInstance(context: Context): AppDatabase {
             synchronized(this) {
                 var instance = INSTANCE
@@ -28,7 +57,10 @@ abstract class AppDatabase : RoomDatabase() {
                         context,
                         AppDatabase::class.java,
                         "foodie_diary_database"
-                    ).build()
+                    )
+                        .addMigrations(MIGRATION_1_2)
+                        .build()
+
                     INSTANCE = instance
                 }
                 return instance
