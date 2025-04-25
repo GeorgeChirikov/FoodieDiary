d5fa1725c8693e32e2d014b9574e7e3a73bb46b6
Cindy <cindy.zhou@metropolia.fi>
2025-04-25 19:07:00 +0300

    Cleaner basic popup

--- app/src/main/java/com/example/foodiediary/views/PopUpView.kt
+++ app/src/main/java/com/example/foodiediary/views/PopUpView.kt
@@ -1,30 +1,85 @@
 package com.example.foodiediary.views
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 
 @Composable
 fun PopUpView(showPopup: Boolean, closePopup: () -> Unit) {
+    var isVisible by remember { mutableStateOf(showPopup) }
+    val alpha: Float by animateFloatAsState(
+        targetValue = if (isVisible) 1f else 0f,
+        animationSpec = tween(durationMillis = 300),
+        label = "alpha"
+    )
+
+    val scale: Float by animateFloatAsState(
+        targetValue = if (isVisible) 1f else 0.8f,
+        animationSpec = tween(durationMillis = 300),
+        label = "scale"
+    )
+
     if (showPopup) {
-        Popup(
-            onDismissRequest = closePopup,
-        ) {
-            Column(
-                modifier = Modifier
-                    .background(Color.White)
-                    .padding(16.dp)
+        if(alpha == 0f) {
+            LaunchedEffect(Unit){
+                closePopup()
+            }
+        } else {
+            Dialog(
+                onDismissRequest = {
+                    isVisible = false
+                    closePopup()
+                },
+                properties = DialogProperties(usePlatformDefaultWidth = false, dismissOnClickOutside = true)
             ) {
-                Text("This is a popup!")
-                Button(onClick = closePopup) {
-                    Text("Close")
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black.copy(alpha = 0.5f)),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .size(250.dp)
+                            .scale(scale)
+                            .alpha(alpha)
+                            .background(Color.White)
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text("This is a centered popup!")
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Button(onClick = {
+                            isVisible = false
+                        }) {
+                            Text("Close")
+                        }
+                    }
                 }
             }
         }
