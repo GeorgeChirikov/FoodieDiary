2e50f303af814dc0e9e6f41d3db387cb977daefb
maisajulianna <maisa.osterman@gmail.com>
2025-05-07 23:09:44 +0300

    favorites ui updates

--- app/src/main/java/com/example/foodiediary/views/FavoritesView.kt
+++ app/src/main/java/com/example/foodiediary/views/FavoritesView.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
